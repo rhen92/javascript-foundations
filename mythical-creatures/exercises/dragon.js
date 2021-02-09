b85dc7e70b5964442acef2145194a6fd10057493
Rachel Hendricks <>
2021-02-08 21:17:40 -0700

    Add dragon.js

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+  constructor(name, rider, hungry) {
+    this.name = name;
+    this.rider = rider;
+    this.hungry = true;
+    this.mealsEaten = 0;
+  }
 
+  greet() {
+    return `Hi, ${this.rider}!`;
+  }
+  eat() {
+    this.mealsEaten += 1
+    if(this.mealsEaten <= 2) {
+      this.hungry = true
+    } else{
+      this.hungry = false
+    }
+    }
+  }
+
+
+module.exports = Dragon
