5ee4f6017469919956eb2ea13c88690851b58908
Rachel Hendricks <>
2021-02-15 15:18:40 -0700

    Add medusa.js

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,21 @@
 var Person = require('./person');
 var Statue = require('./statue');
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+  gazeAtVictim(person) {
+    var statue = new Statue(person.name);
+    if(this.statues.length < 3) {
+    this.statues.push(statue);
+  } else {
+    var freedPerson = this.statues.shift();
+    this.statues.push(statue);
+    return freedPerson.name = new Person(freedPerson.name, 'relieved');
+  }
+  }
+}
+
+module.exports = Medusa;
