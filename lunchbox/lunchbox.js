559f88917312ed0486400f73de21405963e57b58
Rachel Hendricks <>
2021-02-17 21:42:03 -0700

    Add lunchbox.js

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,25 @@
 class LunchBox {
+  constructor(box) {
+    this.owner = box.owner;
+    this.material = box.madeOf;
+    this.shape = box.shape;
+    this.color = box.color;
+    this.snacks = [];
+    this.healthySnacks = [];
+  }
+  acquireSnack(fruitSnack) {
+    this.snacks.push(fruitSnack);
+    }
+  findHealthySnacks() {
+    for(var i = 0; i < this.snacks.length; i++) {
+      if(this.snacks[i].type.toLowerCase().includes('fruit')) {
+        this.healthySnacks.push(this.snacks[i].type);
+      }
+    }
+    return this.healthySnacks;
+  }
+  }
 
-}
+
 
 module.exports = LunchBox;
