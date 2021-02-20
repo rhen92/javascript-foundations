fc233c720c8b9d4459e2e5d960c40b8bed5fb77e
Rachel Hendricks <>
2021-02-20 10:36:08 -0700

    Add snowman.js

--- do-you-wanna-build-a-snowman/src/Snowman.js
+++ do-you-wanna-build-a-snowman/src/Snowman.js
@@ -1,5 +1,24 @@
 class Snowman {
-
+  constructor(snowman) {
+    this.carrots = snowman.carrots;
+    this.coal = snowman.coal;
+    this.buttons = snowman.buttons;
+    this.snowballs = snowman.snowballs;
+    this.magicHat = false;
+  }
+  canWearMagicHat() {
+    if(this.coal < 2) {
+      this.magicHat = false;
+    } else if(this.buttons < 5) {
+      this.magicHat = false;
+    } else if (this.carrots < 1) {
+      this.magicHat = false;
+    } else if (this.snowballs < 2) {
+      this.magicHat = false;
+    } else {
+      this.magicHat = true;
+    }
+}
 }
 
 module.exports = Snowman;
