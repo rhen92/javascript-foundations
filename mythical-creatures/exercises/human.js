b6597656ea1e13dd88a5752106efe19d641dc717
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-01 08:54:58 -0700

    Add human

---
+++ mythical-creatures/exercises/human.js
@@ -0,0 +1,18 @@
+class Human {
+  constructor(name) {
+    this.name = name;
+    this.encounterCounter = 0;
+    this.knockedOut = false;
+  }
+  noticesOgre() {
+    if(this.encounterCounter === 3) {
+      return true;
+    } else if(this.encounterCounter === 6){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+module.exports = Human;
