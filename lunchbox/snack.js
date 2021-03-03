605405b160724b3901008ffa006cc3cd0c54f2dd
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-03 15:01:32 -0700

    Add snack

--- lunchbox/snack.js
+++ lunchbox/snack.js
@@ -1,5 +1,27 @@
 class Snack {
-
+  constructor(object) {
+    this.deliciousLevel = 'extra';
+    this.type = object;
+    this.amount = 100;
+    this.isInLunchBox = false;
+    this.cuttingItClose = false;
+    this.healthy = false;
+  }
+  getEaten() {
+    this.amount -= 10;
+    if(this.amount <= 20) {
+      this.cuttingItClose = true;
+    }
+  }
+  checkForHealthy() {
+    if(this.type === 'Fruit') {
+      this.healthy = true;
+    } else if(this.type === 'Mixed fruit') {
+      this.healthy = true;
+    } else {
+      this.healthy = false;
+    }
+  }
 }
 
 module.exports = Snack;
