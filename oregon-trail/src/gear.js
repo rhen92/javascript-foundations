a2bf1feca0d81bb3c1662ebfa3e48556a2c05f80
Rachel Hendricks <rachelhen92@gmail.com>
2021-03-03 15:03:11 -0700

    Add gear

--- oregon-trail/src/gear.js
+++ oregon-trail/src/gear.js
@@ -1,5 +1,16 @@
 class Gear {
+ constructor(type, quantity) {
+   this.type = type;
+   this.quantity = quantity
+ }
 
+ checkForValidity() {
+   if(this.type === 'ammunition' || this.type === 'food' || this.type === 'clothes') {
+     return `Great, we\'ll need lots of ${this.type}!`;
+   } else {
+     return `I don\`t think a ${this.type} will help us.`;
+   }
+ }
 }
 
 module.exports = Gear;
